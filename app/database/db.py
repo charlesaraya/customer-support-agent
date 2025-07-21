460afdb3b4b410d5e0b6a1406e0a338a0203e404
charlesaraya <charlesaraya@gmail.com>
2025-07-21 16:12:06 +0200

    fix: enable foreign keys on each connection

--- app/database/db.py
+++ app/database/db.py
@@ -1,11 +1,19 @@
 import os
 
-from sqlmodel import SQLModel, create_engine, Session
+from sqlmodel import create_engine, Session
+from sqlalchemy import event
+
 from dotenv import load_dotenv
 load_dotenv()
 
 sqlite_file = os.environ.get("SQLITE_DB_NAME")
 engine = create_engine(f"sqlite:///{sqlite_file}", echo=True)
+
+@event.listens_for(engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys = ON")
+    cursor.close()
 
 def get_session():
     with Session(engine) as session:
